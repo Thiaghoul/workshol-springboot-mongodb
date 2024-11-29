2b0bf1a38fdf344ae144da84f84777de683f067d
thiaghoul <thiagohnunes.w@gmail.com>
2024-11-29 20:44:10 -0300

    Simple query with @query

--- src/main/java/com/thiaghoul/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/thiaghoul/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.thiaghoul.workshopmongo.repositories;
 import com.thiaghoul.workshopmongo.domain.Post;
 import com.thiaghoul.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,8 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String title);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
 }
